518fc8cda07e527a2096b3e010e185378ce7c203
blackbird17922se <blackbird17922@gmail.com>
2026-08-04 21:24:43 -0500

    inicio rdstore

---
+++ src/main/java/com/dsd/rdstore/RdstoreApplication.java
@@ -0,0 +1,13 @@
+package com.dsd.rdstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RdstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RdstoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dsd/rdsd/RdsdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dsd.rdsd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RdstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
