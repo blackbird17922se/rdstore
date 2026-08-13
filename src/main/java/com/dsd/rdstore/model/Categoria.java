27f202b870200267241fbdcc1c400db04e18aaea
blackbird17922se <blackbird17922@gmail.com>
2026-08-13 00:45:07 -0500

    Inicio C,R Categoria

---
+++ src/main/java/com/dsd/rdstore/model/Categoria.java
@@ -0,0 +1,21 @@
+package com.dsd.rdstore.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Table(name = "tipo_producto")
+@Data
+public class Categoria {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nombre;
+    
+}
---
+++ src/main/java/com/dsd/rdstore/repository/CategoriaRepository.java
@@ -0,0 +1,7 @@
+package com.dsd.rdstore.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.dsd.rdstore.model.Categoria;
+
+public interface CategoriaRepository extends JpaRepository<Categoria, Long>{}
