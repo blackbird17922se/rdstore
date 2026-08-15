ef1e2b5b4bca0f3f7fb6c38d4aed0324061a121b
blackbird17922se <blackbird17922@gmail.com>
2026-08-14 21:42:44 -0500

    Usuario obtenerXid, cambiar contraseña, cambiar estado, actualizar

--- src/main/java/com/dsd/rdstore/controller/UsuarioController.java
+++ src/main/java/com/dsd/rdstore/controller/UsuarioController.java
@@ -3,8 +3,11 @@ package com.dsd.rdstore.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.dsd.rdstore.dto.usuario.UsuarioEstadoDTO;
+import com.dsd.rdstore.dto.usuario.UsuarioPasswordDTO;
 import com.dsd.rdstore.dto.usuario.UsuarioRequestDTO;
 import com.dsd.rdstore.dto.usuario.UsuarioResponseDTO;
+import com.dsd.rdstore.dto.usuario.UsuarioUpdateDTO;
 import com.dsd.rdstore.service.UsuarioService;
 
 import jakarta.validation.Valid;
@@ -15,7 +18,10 @@ import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
@@ -38,5 +44,43 @@ public class UsuarioController {
         
         return ResponseEntity.status(HttpStatus.CREATED).body(nuevoUsuario);
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UsuarioResponseDTO> obtenerUsuarioPorId(@PathVariable Long id){
+        return ResponseEntity.ok(usuarioService.obtenerUsuarioPorId(id));
+    }
+
+
+    @PutMapping("/{id}")
+    public ResponseEntity<UsuarioResponseDTO> actualizarUsuario(
+        @PathVariable Long id,
+        @Valid @RequestBody UsuarioUpdateDTO dto
+    ){
+        UsuarioResponseDTO usuarioActualizado = usuarioService.actualizarUsuario(dto, id);
+
+        return ResponseEntity.ok(usuarioActualizado);
+    }
+
+    @PatchMapping("/{id}/estado")
+    public ResponseEntity<UsuarioResponseDTO> cambiarEstadoUsuario(
+            @PathVariable Long id, @Valid @RequestBody UsuarioEstadoDTO dto) {
+
+        UsuarioResponseDTO usuarioActualizado = usuarioService.cambiarEstadoUsuario(id, dto);
+
+        return ResponseEntity.ok(usuarioActualizado);
+
+    }
+
+    @PatchMapping("/{id}/contrasena")
+    public ResponseEntity<UsuarioResponseDTO> cambiarContrasenaUsuario(
+            @PathVariable Long id, @Valid @RequestBody UsuarioPasswordDTO dto) {
+
+        UsuarioResponseDTO usuarioActualizado = usuarioService.cambiarContrasenaUsuario(id, dto);
+
+        return ResponseEntity.ok(usuarioActualizado);
+
+    }
+
+
 
 }
--- src/main/java/com/dsd/rdstore/dto/usuario/UsuarioUpdateDTO.java
+++ src/main/java/com/dsd/rdstore/dto/usuario/UsuarioUpdateDTO.java
@@ -3,6 +3,5 @@ package com.dsd.rdstore.dto.usuario;
 public record UsuarioUpdateDTO(
         String nombre,
         String apellido,
-        Boolean activo,
         Long idRol
 ) {}
